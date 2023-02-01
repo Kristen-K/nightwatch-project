b8f262501bcb9d0136ce8ac819e5621952587440
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-01 14:35:07 +0000

    second test added

--- test/specs/homepage.js
+++ test/specs/homepage.js
@@ -5,12 +5,16 @@ describe("Home", () => {
     it("Open URL & assert test", async () => {
         const homePage = browser.page.homePage();
         homePage.navigate().assert.title('Practice E-Commerce Site – Automation Bro')
+        browser.end;
   
     });
-    // it ("Open About page & assert test", async () => {
-    //     homePage.navigate()
-    // })
-  
+    it ("Open About page & assert test", async () => {
+        const aboutPage = browser.navigateTo("https://practice.automationbro.com/about/");
+        aboutPage.assert.urlEquals('https://practice.automationbro.com/about/')
+    })
+
+    
 
     
   });
+
