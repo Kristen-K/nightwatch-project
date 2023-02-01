29f8cb120ffea8e9021ea1058b67290a5fb7b677
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-01 16:27:12 +0000

    logo assert

--- test/specs/homepage.js
+++ test/specs/homepage.js
@@ -1,16 +1,37 @@
 
 
-describe("Home", () => {
+describe("Home", function() {
+
+    // before(function(browser) {
+    //     this.homepage = browser.page.homePage();
+    // })
 
     it("Open URL & assert test", async () => {
         const homePage = browser.page.homePage();
         homePage.navigate().assert.title('Practice E-Commerce Site – Automation Bro')
-        browser.end;
+        //browser.end;
   
     });
-    it ("Open About page & assert test", async () => {
+    it("Open About page & assert test", async () => {
         const aboutPage = browser.navigateTo("https://practice.automationbro.com/about/");
         aboutPage.assert.urlEquals('https://practice.automationbro.com/about/')
+    });
+    it("Click get started button & assert URL contains get-started text", async () => {
+        const homePage = browser.page.homePage()
+        homePage
+        .navigate()
+        .click('@btnGetStarted')
+        .assert.urlContains('get-started')
+    });
+    it("Click on logo button & assert URL does not contain get-started text", async () => {
+       const homePage = browser.page.homePage()
+       homePage
+       .navigate()
+       // not working
+       //.click('@imageLogo')
+        .click('img[alt="Practice E-Commerce Site"]')
+        .assert.not.urlContains('get-started')
+
     })
 
     
