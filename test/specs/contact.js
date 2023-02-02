a0fb674ac35f62426c2f5703a9bc10c31c4abada
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-02 17:59:41 +0000

    contact page complete

--- test/specs/contact.js
+++ test/specs/contact.js
@@ -4,6 +4,15 @@ describe('Fill in Contact input fields, submit form & assert success message', (
         const contactPage = browser.page.contactPage()
         contactPage
         .navigate()
+        .updateValue('@inputName', 'Simon Boot')
+        .updateValue('@inputEmail', 'hshsh@hh.com')
+        .updateValue('@inputPhoneNumber', '00904000000')
+        .click('@btnSubmit')
+        //try submit form .submitForm()
+    })
 
+    it('assert successs message', async () => {
+        const contactPage = browser.page.contactPage()
+        .assert.textEquals('@alertRole', 'Thanks for contacting us! We will be in touch with you shortly')
     })
 })
