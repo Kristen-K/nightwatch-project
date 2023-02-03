05f9026771527399fbe1bb6ac00455d60d3a4c06
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-03 16:17:35 +0000

    added loop

--- test/specs/blog.js
+++ test/specs/blog.js
@@ -1,11 +1,17 @@
+
 describe('Get list of recent posts, check they are >10 characters & total of 5 posts', () => {
     it('Get list of recent posts', async () => {
         const blogPage = browser.page.blogPage()
         blogPage
         .navigate()
-        browser.findElements('#recent-posts-3 ul li')
-        browser.expect.elements('#recent-posts-3 ul li').count.to.equal(5);
-        //console.log(list);
-
+        .expect.elements('@blogList').count.to.equal(5)
+    
+        const list = await browser.findElements('#recent-posts-3 ul li')
+        //console.log("here:", list)
+        for (var i = 0; i < list.length; i++) {
+            let items = await browser.getText(list[i])
+            console.log("here:", items )
+            
+        }
     })
 })
