860184a4da7f2e05f2f818e98f5a129e8b7f14b7
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-06 18:29:15 +0000

    Added faker info

--- test/specs/contact.js
+++ test/specs/contact.js
@@ -1,18 +1,30 @@
+const { faker } = require('@faker-js/faker');
 
 describe('Fill in Contact input fields, submit form & assert success message', () => {
     it('fill in name, email, phone & message', async () => {
+    
+
+
+        const  name = faker.name.fullName();
+        const email = faker.internet.email();
+        const phoneNumber = faker.phone.number();
+        const message = faker.lorem.paragraphs(2);
+        console.log("here:", name, email, phoneNumber, message)
+    
         const contactPage = browser.page.contactPage()
         contactPage
         .navigate()
-        .updateValue('@inputName', 'Simon Boot')
-        .updateValue('@inputEmail', 'hshsh@hh.com')
-        .updateValue('@inputPhoneNumber', '00904000000')
+        .updateValue('@inputName', name)
+        .updateValue('@inputEmail', email)
+        .updateValue('@inputPhoneNumber', phoneNumber)
+        .updateValue('@textAreaMessage', message)
         .click('@btnSubmit')
-        //try submit form .submitForm()
     })
 
     it('assert successs message', async () => {
         const contactPage = browser.page.contactPage()
+        contactPage
         .assert.textEquals('@alertRole', 'Thanks for contacting us! We will be in touch with you shortly')
     })
 })
+
