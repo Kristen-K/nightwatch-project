d1c7e02d55fe74e7b9f5ca03bac5a6e88b087ebc
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-26 12:48:25 +0000

    linting

--- nightwatch/page-objects/homePage.js
+++ nightwatch/page-objects/homePage.js
@@ -1,17 +1,15 @@
-//const about = '/about/';
-
 module.exports = {
-    url: 'https://practice.automationbro.com',
+  url: "https://practice.automationbro.com",
 
-    elements: {
-        btnGetStarted: {
-            selector: '#get-started'
-        },
-        imageLogo: {
-            selector: 'img[alt="Practice E-Commerce Site"]'
-        },
-        txtHeading: {
-            selector: '.elementor-widget-container h1'
-        }
-    }
-}
+  elements: {
+    btnGetStarted: {
+      selector: "#get-started",
+    },
+    imageLogo: {
+      selector: 'img[alt="Practice E-Commerce Site"]',
+    },
+    txtHeading: {
+      selector: ".elementor-widget-container h1",
+    },
+  },
+};
--- test/specs/homepage.js
+++ test/specs/homepage.js
@@ -1,35 +1,32 @@
-describe("Home", function() {
-    it("Open URL & assert test", async () => {
-        const homePage = browser.page.homePage();
-        homePage.navigate().assert.title('Practice E-Commerce Site – Automation Bro')
-        //browser.end;
-  
-    });
-    it("Open About page & assert test", async () => {
-        const aboutPage = browser.navigateTo('/about/');
-        aboutPage.assert.urlEquals('https://practice.automationbro.com/about/')
-    });
-    it("Click get started button & assert URL contains get-started text", async () => {
-        const homePage = browser.page.homePage()
-        homePage
-        .navigate()
-        .click('@btnGetStarted')
-        .assert.urlContains('get-started')
-    });
-    it("Click on logo button & assert URL does not contain get-started text", async () => {
-       const homePage = browser.page.homePage()
-       homePage
-       .navigate()
-       .click('@imageLogo')
-        .assert.not.urlContains('get-started')
-
-    })
-    it("Find heading element & assert the text", async () => {
-        const homePage = browser.page.homePage()
-        homePage
-        .navigate()
-        .assert.textEquals('@txtHeading', 'Think different. Make different.')
-    })
-    
+describe("Home", function () {
+  it("Open URL & assert test", async () => {
+    const homePage = browser.page.homePage();
+    homePage
+      .navigate()
+      .assert.title("Practice E-Commerce Site – Automation Bro");
   });
-
+  it("Open About page & assert test", async () => {
+    const aboutPage = browser.navigateTo("/about/");
+    aboutPage.assert.urlEquals("https://practice.automationbro.com/about/");
+  });
+  it("Click get started button & assert URL contains get-started text", async () => {
+    const homePage = browser.page.homePage();
+    homePage
+      .navigate()
+      .click("@btnGetStarted")
+      .assert.urlContains("get-started");
+  });
+  it("Click on logo button & assert URL does not contain get-started text", async () => {
+    const homePage = browser.page.homePage();
+    homePage
+      .navigate()
+      .click("@imageLogo")
+      .assert.not.urlContains("get-started");
+  });
+  it("Find heading element & assert the text", async () => {
+    const homePage = browser.page.homePage();
+    homePage
+      .navigate()
+      .assert.textEquals("@txtHeading", "Think different. Make different.");
+  });
+});
