4937e3aea6fef8ab5a8b155863977c745365cbaf
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-26 12:25:12 +0000

    linting

--- test/specs/contact.js
+++ test/specs/contact.js
@@ -1,30 +1,27 @@
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 
-describe('Fill in Contact input fields, submit form & assert success message', () => {
-    it('fill in name, email, phone & message', async () => {
-    
+describe("Fill in Contact input fields, submit form & assert success message", () => {
+  it("fill in name, email, phone & message", async () => {
+    const name = faker.name.fullName();
+    const email = faker.internet.email();
+    const phoneNumber = faker.phone.number();
+    const message = faker.lorem.paragraphs(2);
 
+    const contactPage = browser.page.contactPage();
+    contactPage
+      .navigate()
+      .updateValue("@inputName", name)
+      .updateValue("@inputEmail", email)
+      .updateValue("@inputPhoneNumber", phoneNumber)
+      .updateValue("@textAreaMessage", message)
+      .click("@btnSubmit");
+  });
 
-        const  name = faker.name.fullName();
-        const email = faker.internet.email();
-        const phoneNumber = faker.phone.number();
-        const message = faker.lorem.paragraphs(2);
-        console.log("here:", name, email, phoneNumber, message)
-    
-        const contactPage = browser.page.contactPage()
-        contactPage
-        .navigate()
-        .updateValue('@inputName', name)
-        .updateValue('@inputEmail', email)
-        .updateValue('@inputPhoneNumber', phoneNumber)
-        .updateValue('@textAreaMessage', message)
-        .click('@btnSubmit')
-    })
-
-    it('assert successs message', async () => {
-        const contactPage = browser.page.contactPage()
-        contactPage
-        .assert.textEquals('@alertRole', 'Thanks for contacting us! We will be in touch with you shortly')
-    })
-})
-
+  it("assert successs message", async () => {
+    const contactPage = browser.page.contactPage();
+    contactPage.assert.textEquals(
+      "@alertRole",
+      "Thanks for contacting us! We will be in touch with you shortly"
+    );
+  });
+});
