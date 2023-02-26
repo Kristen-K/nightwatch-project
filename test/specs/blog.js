3aa9e51eff948d9d4364525b47b67b41d3c4b2ca
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-26 12:35:25 +0000

    linting

--- test/specs/blog.js
+++ test/specs/blog.js
@@ -1,17 +1,15 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 
-describe('Get list of recent posts, check they are >10 characters & total of 5 posts', () => {
-    it('Get list of recent posts', async () => {
-        const blogPage = browser.page.blogPage()
-        blogPage
-        .navigate()
-        .expect.elements('@blogList').count.to.equal(5)
-    
-        const list = await browser.findElements('#recent-posts-3 ul li')
+describe("Get list of recent posts, check they are >10 characters & total of 5 posts", () => {
+  it("Get list of recent posts", async () => {
+    const blogPage = browser.page.blogPage();
+    blogPage.navigate().expect.elements("@blogList").count.to.equal(5);
 
-        for (let i = 0; i < list.length; i++) {
-            let items = await browser.getText(list[i])
-            expect(items).to.have.length.above(10);
-        }
-    })
-})
+    const list = await browser.findElements("#recent-posts-3 ul li");
+
+    for (let i = 0; i < list.length; i++) {
+      let items = await browser.getText(list[i]);
+      expect(items).to.have.length.above(10);
+    }
+  });
+});
